9182904698bb2c56f9ac1292fa3d5c246c3b0e98
Oleksandr Latyntsev <Oleksnadr.latyntsev@intigral.com>
2015-07-08 14:52:23 +0400

    init

---
+++ DGTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DGTestProject
+//
+//  Created by Oleksandr Latyntsev on 7/8/15.
+//  Copyright (c) 2015 DGTestProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DGTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DGTestProject
+//
+//  Created by Oleksandr Latyntsev on 7/8/15.
+//  Copyright (c) 2015 DGTestProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
